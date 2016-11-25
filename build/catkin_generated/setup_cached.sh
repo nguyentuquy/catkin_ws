7fcd342474f77259461cceb7b113b625270463b6
Nguyen Tu Quy <nguyentuquy2008@gmail.com>
2016-11-25 22:06:55 +0100

    catkin_ws started

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/quy/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/indigo/lib:/opt/qt57/lib/x86_64-linux-gnu:/opt/qt57/lib"
+export PATH="/opt/ros/indigo/bin:/opt/qt57/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin"
+export PKG_CONFIG_PATH="/opt/ros/indigo/lib/pkgconfig:/opt/qt57/lib/pkgconfig"
+export PWD="/home/quy/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/quy/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/quy/catkin_ws/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
