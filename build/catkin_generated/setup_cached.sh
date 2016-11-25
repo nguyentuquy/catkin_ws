245bf027d0ab1b0b467aa2a226383d4aa1b618c4
Nguyen Tu Quy <nguyentuquy2008@gmail.com>
2016-11-25 22:11:14 +0100

    create new pkg

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,10 +7,7 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/quy/catkin_ws/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/opt/ros/indigo/lib:/opt/qt57/lib/x86_64-linux-gnu:/opt/qt57/lib"
-export PATH="/opt/ros/indigo/bin:/opt/qt57/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin"
+export CPATH="/opt/ros/indigo/include"
+export LD_LIBRARY_PATH="/home/quy/catkin_ws/devel/lib:/opt/ros/indigo/lib:/opt/qt57/lib/x86_64-linux-gnu:/opt/qt57/lib"
 export PKG_CONFIG_PATH="/opt/ros/indigo/lib/pkgconfig:/opt/qt57/lib/pkgconfig"
-export PWD="/home/quy/catkin_ws/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/quy/catkin_ws/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/quy/catkin_ws/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
+export PYTHONPATH="/opt/ros/indigo/lib/python2.7/dist-packages"
