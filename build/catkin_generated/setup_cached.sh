6ffc28bbaeea2d4558f40cc205c3d13e659236be
Nguyen Tu Quy <nguyentuquy2008@gmail.com>
2016-11-26 16:33:15 +0100

    aaaa

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,5 +7,9 @@
 # new environment variables
 
 # modified environment variables
-export PKG_CONFIG_PATH="/home/quy/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export CMAKE_PREFIX_PATH="/home/quy/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/indigo/lib:/opt/qt57/lib/x86_64-linux-gnu:/opt/qt57/lib:/home/quy/catkin_ws/devel/lib"
+export PATH="/opt/ros/indigo/bin:/opt/qt57/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin"
+export PKG_CONFIG_PATH="/opt/ros/indigo/lib/pkgconfig:/opt/qt57/lib/pkgconfig:/home/quy/catkin_ws/devel/lib/pkgconfig"
 export PWD="/home/quy/catkin_ws/build"
+export ROS_PACKAGE_PATH="/home/quy/catkin_ws/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
